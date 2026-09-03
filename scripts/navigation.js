1fecd84d75fdcccfc22a8235ba73550d575b61c0
Manuel Chave <manuelmariomanuel199@gmail.com>
2026-09-03 16:09:59 +0200

    fixid some accessibility error reported from lighthouse

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,5 +4,6 @@ const navBar = document.querySelector("#nav-bar")
 
 btnHumburger.addEventListener("click", () => {
     btnHumburger.classList.toggle("show")
+    btnHumburger.ariaLabel = "Close menu"
     navBar.classList.toggle("show")
 })
