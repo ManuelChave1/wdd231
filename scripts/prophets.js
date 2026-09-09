1900bdb92f1ac35d82463838b21c3e3e4517ba27
Manuel Chave <manuelmariomanuel199@gmail.com>
2026-09-09 07:00:13 +0200

    I have worked on prophets media query and retified some css

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -19,13 +19,19 @@ const displayProphets = (prophets) => {
         let card = document.createElement("section");
         let fullName = document.createElement("h2");
         let portrait = document.createElement("img");
+        let dateBirth = document.createElement("p")
+        let placeBirth = document.createElement("p")
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        dateBirth.textContent = `Date of Birth: ${prophet.birthdate}`
+        placeBirth.textContent = `Place of Birth: ${prophet.birthplace}`
         portrait.setAttribute("src", prophet.imageurl)
         portrait.setAttribute("alt", `${prophet.name} ${prophet.lastname} portrait `);
         portrait.setAttribute("loading", "lazy")
         portrait.setAttribute("width", "200px")
         portrait.setAttribute("higth", "250px")
         card.appendChild(fullName)
+        card.appendChild(dateBirth)
+        card.appendChild(placeBirth)
         card.appendChild(portrait)
         cards.appendChild(card)
 
