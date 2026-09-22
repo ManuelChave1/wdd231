14235a8d1dad720e24600c46c375bae5c7351b8d
Manuel Chave <manuelmariomanuel199@gmail.com>
2026-09-22 23:12:39 +0200

    I have worked processing the information from the form on the thankyou page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -5,35 +5,37 @@ const joinForm = document.querySelector("#joinForm")
 const params = new URLSearchParams(window.location.search)
 
 
+const aplicationInfor = document.querySelector("#aplication-infor")
+const divAplication = document.createElement("div")
+divAplication.classList.add("divAplication")
 
-if (joinForm) {
-    const aplicationInfor = document.querySelector("#aplication-infor")
-    const divAplication = document.createElement("div")
-    divAplication.classList.add("divAplication")
-
+if (aplicationInfor) {
     const fullName = document.createElement("p")
     const organizationTitle = document.createElement("p")
+    const organizationName = document.createElement("p")
     const email = document.createElement("p")
     const phoneNumber = document.createElement("p")
     const description = document.createElement("p")
+    const memberLevel = document.createElement("p")
 
     fullName.textContent = `Name: ${params.get('first')} ${params.get('last')}`;
     email.textContent = `Email: ${params.get('email')}`
     phoneNumber.textContent = `Phone Number: ${params.get('phoneNumber')}`
-    organizationTitle.textContent = `Organization: ${params.get('organization')}`
-    description.textContent = `Organization: ${params.get('organizationDescription')}`
+    organizationTitle.textContent = `Organization title: ${params.get('organizationTitle')}`
+    organizationName.textContent = `Organization name: ${params.get('organizationTitle')}`
+    description.textContent = `Description: ${params.get('organizationDescription')}`
+    memberLevel.textContent = `Member Level: ${params.get('membershipLevel')}`
 
     divAplication.append(
         fullName,
         email,
         phoneNumber,
         organizationTitle,
+        organizationName,
+        memberLevel,
         description
     )
 
     aplicationInfor.appendChild(divAplication)
+}
 
-}
-else {
-    alert("Error processing the form")
-}
