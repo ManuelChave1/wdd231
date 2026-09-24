3c20187889e87a2c95bded5fa4367b7cd2e7501f
Manuel Chave <manuelmariomanuel199@gmail.com>
2026-09-24 21:37:06 +0200

    I have worked organizing the  media query of the page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,5 +1,4 @@
 
-const btnSubmint = document.querySelector("#btnSubmint")
 const timeStamp = document.querySelector("#timestamp")
 const joinForm = document.querySelector("#joinForm")
 const params = new URLSearchParams(window.location.search)
@@ -18,13 +17,13 @@ if (aplicationInfor) {
     const description = document.createElement("p")
     const memberLevel = document.createElement("p")
 
-    fullName.textContent = `Name: ${params.get('first')} ${params.get('last')}`;
-    email.textContent = `Email: ${params.get('email')}`
-    phoneNumber.textContent = `Phone Number: ${params.get('phoneNumber')}`
-    organizationTitle.textContent = `Organization title: ${params.get('organizationTitle')}`
-    organizationName.textContent = `Organization name: ${params.get('organizationTitle')}`
-    description.textContent = `Description: ${params.get('organizationDescription')}`
-    memberLevel.textContent = `Member Level: ${params.get('membershipLevel')}`
+    fullName.innerHTML = `<strong>Name: </strong> ${params.get('first')} ${params.get('last')}`;
+    email.innerHTML = `<strong>Email: </strong> ${params.get('email')}`
+    phoneNumber.innerHTML = `<strong>Phone Number: </strong> ${params.get('phoneNumber')}`
+    organizationTitle.innerHTML = `<strong>Organization title:</strong> ${params.get('organizationTitle')}`
+    organizationName.innerHTML = `<strong>Organization name: </strong>${params.get('organizationTitle')}`
+    description.innerHTML = `<strong>Description:</strong> ${params.get('organizationDescription')} `
+    memberLevel.innerHTML = `<strong>Member Level: </strong> ${params.get('membershipLevel')}`
 
     divAplication.append(
         fullName,
